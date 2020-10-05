e4cef8efa887757834620c12d16e5689cdfacc72
折影轻梦 <776194970@qq.com>
2020-10-05 08:47:34 +0800

    setmode bug fixed

--- routers/routers.go
+++ routers/routers.go
@@ -1,22 +1,22 @@
 package routers
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
-    "gin-starter/controllers/api/v1"
-    "gin-starter/pkg/setting"
+	v1 "gin-starter/controllers/api/v1"
+	"gin-starter/pkg/setting"
 )
 
 func InitRouter() *gin.Engine {
-    r := gin.Default()
+	gin.SetMode(setting.Cfg.GetString("run_mode"))
 
-    gin.SetMode(setting.Cfg.GetString("run_mode"))
+	r := gin.Default()
 
-    apiv1 := r.Group("/api/v1")
-    {
-        //获取标签列表
-        apiv1.GET("/contents", v1.GetContent)
-    }
+	apiv1 := r.Group("/api/v1")
+	{
+		//获取标签列表
+		apiv1.GET("/contents", v1.GetContent)
+	}
 
-    return r
+	return r
 }
